2d3ae7969e6b71eaaa0aa2347f875ef98a34c5e6
Dominic Tarr <dominic.tarr@gmail.com>
2012-11-13 21:08:04 -0800

    expose first, last, middle, and places names

--- index.js
+++ index.js
@@ -1,13 +1,28 @@
 
 var names = require('./names.json')
+var first = require('./first-names.json')
+var middle = require('./middle-names.json')
+var place = require('./places.json')
+
+function r(names) {
+  return function () {
+    return names[~~(Math.random()*names.length)]
+  }
+}
 
 var random = module.exports = function () {
-  return names[~~(Math.random()*names.length)]
+  return random.first() + ' ' +random.last()
 }
+
+random.first   = r(first)
+random.last    = r(names)
+random.middle  = r(middle)
+random.place   = r(place)
 
 if(!module.parent) {
   var l = process.argv[2] || 10
   while (l--)
-    console.log(random())
+    console.log(random.first(), '.', random.middle(), '.', random.last()
+    , ',', random.place())
 }
   

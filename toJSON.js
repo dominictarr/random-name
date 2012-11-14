59d8d454c827b774b70f78f1dec3851318dfb906
Dominic Tarr <dominic.tarr@gmail.com>
2012-11-13 21:07:10 -0800

    handle \r\n

--- toJSON.js
+++ toJSON.js
@@ -3,6 +3,6 @@ var es = require('event-stream')
 var JSONStream = require('JSONStream')
 
 process.stdin  
-  .pipe(es.split())
+  .pipe(es.split(/\r?\n/))
   .pipe(JSONStream.stringify())
   .pipe(process.stdout)
